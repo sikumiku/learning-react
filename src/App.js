066987dfbd20dd8756f7666ebb0a36fe70e7ac56
Sigrid Aasma <sigridaasma@gmail.com>
2018-12-12 16:46:05 +0200

    eject and amend to use css modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -73,18 +73,18 @@ class App extends Component {
             style.backgroundColor = 'red';
         }
 
-        let classes = [];
+        let assignedClasses = [];
         if (this.state.persons.length <= 2) {
-            classes.push('red'); // class = ['red']
+            assignedClasses.push( classes.red ); // class = ['red']
         }
         if (this.state.persons.length <= 1) {
-            classes.push('bold'); // class = ['red', 'bold']
+            assignedClasses.push( classes.bold ); // class = ['red', 'bold']
         }
 
         return (
-                <div className="App">
+                <div className={classes.App}>
                     <h1>Hello!</h1>
-                    <p className={classes.join(' ')}>This is really working.</p>
+                    <p className={assignedClasses.join(' ')}>This is really working.</p>
                     <button
                         style={style}
                         onClick={this.togglePersonsHandler}>Toggle persons
