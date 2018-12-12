70d536a1fb297df4f0178bc1320e2ed0c32b0018
Sigrid Aasma <sigridaasma@gmail.com>
2018-12-12 18:51:04 +0200

    use hoc and avoid useless div

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import classes from './Cockpit.css'
+import Aux from '../../hoc/Auxilliary'
 
 const cockpit = (props) => {
 
     const assignedClasses = [];
-    let btnClass = '';
+    let btnClass = classes.Button;
     if(props.showPersons) {
-        btnClass = classes.Red;
+        btnClass = [classes.Button, classes.Red].join(' ');
     }
     if (props.persons.length <= 2) {
         assignedClasses.push( classes.red ); // class = ['red']
@@ -15,16 +16,16 @@ const cockpit = (props) => {
         assignedClasses.push( classes.bold ); // class = ['red', 'bold']
     }
 
+    // Aux is good to use if you dont want to just create a wrapping div
     return (
-        <div className={classes.Cockpit}>
+        <Aux>
             <h1>{props.appTitle}</h1>
             <p className={assignedClasses.join(' ')}>This is really working.</p>
             <button
                 className={btnClass}
                 onClick={props.clicked}>Toggle persons
             </button>
-        </div>
-
+        </Aux>
     );
 };
 
